d308d0014da444ec60f50514728f3493e2ff14cb
GalinaNetishina <netishinagalina@yandex.ru>
2024-07-02 13:39:33 +0300

    Initial commit

---
+++ advertisements/filters.py
@@ -0,0 +1,15 @@
+from django_filters import rest_framework as filters, DateTimeFromToRangeFilter
+
+from advertisements.models import Advertisement
+
+
+class AdvertisementFilter(filters.FilterSet):
+    """Фильтры для объявлений."""
+
+    # TODO: задайте требуемые фильтры
+    created_at = DateTimeFromToRangeFilter()
+    creator = filters.CharFilter(field_name='creator', lookup_expr='exact')
+
+    class Meta:
+        model = Advertisement
+        fields = ['created_at', 'creator', 'status']
